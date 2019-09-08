9cca34f7eaea7295f8e6f94fcaba7041d31d8a3e
mabenteng <84828676@qq.com>
2019-09-09 07:40:03 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/morf', 'IndexController@index');
-Route::get('/home', 'Home\IndexController@index');
+Route::any('/ind', 'Home\IndexController@ind');
+Route::get('/list', 'Home\IndexController@listp');
+Route::any('/xiugai/{id?}', 'Home\IndexController@xiugai');
 //Route::get('/home', 'Home\IndexController@index');
