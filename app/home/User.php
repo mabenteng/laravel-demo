89bbf2ac79d4f524011f37ada96bc709324462bd
mabenteng <84828676@qq.com>
2019-09-11 11:31:04 +0800

    模型commit

--- app/home/User.php
+++ app/home/User.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * @Date: 2019-09-06 06:19:25
+ * @LastEditors: benty
+ * @LastEditTime: 2019-09-11 11:30:30
+ */
+
 
 namespace App\home;
 
@@ -9,5 +15,5 @@ class User extends Model
     public $table='user';
     public $timestamps=false;
     protected $primaryKey='id';
-    //
+    public $guarded=['_token','capt'];
 }
